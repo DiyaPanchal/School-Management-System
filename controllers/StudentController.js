393ace117ae553d5386def55eb9e0c44f91ed5df
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2025-01-21 17:55:14 +0530

    student controller changes

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -7,7 +7,7 @@ export const getStudents = async (req, res) => {
     const students = await Student.find()
       .skip((page - 1) * limit)
       .limit(limit)
-      .populate('class');
+      .populate("class");
     res.json(students);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -34,3 +34,35 @@ export const addStudents = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateStudents = async (req, res) => {
+  const { studentId } = req.params;
+  const { name, age, classId, parentDetails } = req.body;
+
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      studentId,
+      {
+        name,
+        age,
+        class: classId,
+        parentDetails,
+      },
+      { new: true }
+    );
+    res.json(updatedStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteStudent = async (req, res) => {
+    const {studentId} = req.params;
+    try{
+      await Student.findByIdAndDelete(studentId);
+      res.json({message: 'Student deleted successfully'});
+    }
+    catch(error){
+      res.status(500).json({message: error.message});
+    }
+}
